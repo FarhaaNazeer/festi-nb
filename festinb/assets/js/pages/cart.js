d156f63055af85d2f5d50c996929d0ebbcb35187
jeremdh62 <jdhaille@myges.fr>
2022-07-15 13:26:14 +0200

    add qty event

--- festinb/assets/js/pages/cart.js
+++ festinb/assets/js/pages/cart.js
@@ -11,7 +11,7 @@ let cart = () => {
 
     self.init = () => {
         self.deleteItem();
-       // self.updateCart();
+        self.updateCart();
 
        
         if (cartBtnValidate){
@@ -29,34 +29,26 @@ let cart = () => {
             if (element) {
                 element.addEventListener('click', (e) => {
                     let itemUuid = e.target.getAttribute('data-uuid-item');
+                    let itemPrice = parseInt(document.getElementById('item-total-' + itemUuid).innerText);
+                    let totalPrice = parseInt(total.innerText);
+                    let containerCart = document.getElementById('cart-container-'+itemUuid);
 
                     self.sendRequest('app_front_delete_cart', {
                         'ticket': {
                             'uuid': itemUuid,
                         },
                     }).then(function (response) {
-                        const containerCart = document.getElementById('cart-container-'+itemUuid);
-                        containerCart.remove();
-
-                        let itemPrice = parseInt(document.getElementById('item-total-' + itemUuid).innerText);
-
-                        let totalPrice = parseInt(total.innerText);
 
                         let newTotalPrice = totalPrice - itemPrice;
+                        total.textContent = `${newTotalPrice}`;
+                        containerCart.remove();
 
                         if (newTotalPrice == 0) {
-                            document.getElementById('cart-container-total').style.display = 'none';
+                            document.getElementById('container-total').style.display = 'none';
                             let pCartEmpty = document.createElement("p");
                             pCartEmpty.innerHTML = "Aucun article dans ton panier ! Ne perds pas une minute de plus :)";	
-                        
-                            document.getElementById('card-container').appendChild(pCartEmpty);
-                        }
-
-                        total.textContent = `${newTotalPrice}`;
-
-
-
-                        
+                            document.getElementById('container-card').appendChild(pCartEmpty);
+                        }                        
                     });
                 });
             }
@@ -67,18 +59,31 @@ let cart = () => {
         for (let i = 0; i < qtyInputs.length; i++) {
             const element = qtyInputs[i];
             
-            element.addEventListener('change', (e) => {
-                let uuidItem = e.target.getAttribute('data-uuid-item');
-                let itemPrice = parseInt(document.getElementById('item-price-' + uuidItem).innerText);
-                let itemQty = parseInt(e.target.value);
-                let itemTotal = itemPrice * itemQty;
+            if (element) {
+                element.addEventListener('change', (e) => {
+                    let newQty = e.target.value;
+                    let itemUuid = e.target.getAttribute('data-uuid-item');
+                    let itemPrice = parseInt(document.getElementById('item-price-' + itemUuid).value);
+                    let totalItemPrice = document.getElementById('item-total-' + itemUuid);
+                    let newTotalItemPrice = newQty * itemPrice;
 
-                document.getElementById('item-total-' + uuidItem).innerText = itemTotal;
+                    totalItemPrice.innerText = `${newTotalItemPrice}`;
+                    self.updateTotal();
+                  
+                });
+            }
+        }
+    }
 
-                console.log(itemTotal);
-                
-
-            });
+    self.updateTotal = () => {
+        let items = document.getElementsByClassName('container_card__item');
+        let priceTotal = 0;
+        for (let i = 0; i < items.length; i++) {
+            const element = items[i];
+            let uuid = element.getAttribute('data-uuid-item');
+            let itemTotal = parseInt(document.getElementById('item-total-' + uuid).innerText);
+            priceTotal += itemTotal;
+            total.innerText = `${priceTotal}`;
         }
     }
 
